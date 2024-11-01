53ae6b210a39780602e3cdfb1db6b710c4f1f1f5
Kelvinvida99 <kelvinvida99@gmail.com>
2024-10-31 23:21:56 -0400

    modify class person

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,4 +1,6 @@
 (() => {
+  // No aplicando el principio de responsabilidad única
+
   type Gender = "M" | "F";
 
   class Person {
@@ -13,6 +15,51 @@
     }
   }
 
-  const newPerson = new Person('kelvin', "M", new Date(2020-12-12));
+  class User extends Person {
+    public lastAccess: Date;
+
+    constructor(
+      public email: string,
+      public role: string,
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date
+    ) {
+      super(name, gender, birthdate);
+      this.lastAccess = new Date();
+    }
+
+    checkCrendential() {
+      return true;
+    }
+  }
+
+  class UserSetting extends User {
+    constructor(
+      public workingDirectory: string,
+      public lastOpenFolder: string,
+      public email: string,
+      public role: string,
+      public name: string,
+      public gender: Gender,
+      public birthdate: Date
+    ) {
+      super(email, role, name, gender, birthdate);
+      this.workingDirectory = "";
+      this.lastOpenFolder = "";
+    }
+  }
+
+  const newPerson = new Person("kelvin", "M", new Date(2020 - 12 - 12));
   console.log(newPerson);
+
+  const newUserSetting = new UserSetting(
+    "folder",
+    "folder",
+    "kelvin@",
+    "programer",
+    "kelvin",
+    "M",
+    new Date("1999-09-2")
+  )
 })();
